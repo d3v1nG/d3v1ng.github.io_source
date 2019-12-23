92f82c285ccfd1cacdccf99d1479b78b24f88d27
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2019-12-23 09:02:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 function printDevin() {
   ReactDOM.render(<h1>My name is Devin</h1>, document.getElementById('root'));
 }
-export printDevin;
+export default printDevin;
