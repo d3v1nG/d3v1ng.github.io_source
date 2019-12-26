5bea68742eccc6a5d33807f437d9e822ed649c17
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2019-12-26 01:07:10 -0600

    Updates

---
+++ precache-manifest.998a52ea4be9d0bc21eab2ae07c4ff1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717c8f200e2fb9a35220def92ca6eebd",
+    "url": "/my_page/index.html"
+  },
+  {
+    "revision": "7369b1da608a8df75114",
+    "url": "/my_page/static/css/main.c0d7003a.chunk.css"
+  },
+  {
+    "revision": "cb8db210fe2d8b3bb879",
+    "url": "/my_page/static/js/2.3e672ec7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_page/static/js/2.3e672ec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7369b1da608a8df75114",
+    "url": "/my_page/static/js/main.7a229db0.chunk.js"
+  },
+  {
+    "revision": "a06b16cd30d9b71ed13d",
+    "url": "/my_page/static/js/runtime-main.aa5702ad.js"
+  },
+  {
+    "revision": "a3a70418b69298c056a8cfede1b85cf2",
+    "url": "/my_page/static/media/city8bit.a3a70418.jpg"
+  },
+  {
+    "revision": "418f72bb6a64ce2d53984697c62825cd",
+    "url": "/my_page/static/media/pp2.418f72bb.jpg"
+  }
+]);
