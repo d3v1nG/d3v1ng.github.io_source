43ffc1bf0b969454df0f0e5d1fdcf4155289f756
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2020-01-01 23:22:50 -0600

    content/design

--- my_page/src/Page.js
+++ my_page/src/Page.js
@@ -9,24 +9,32 @@ class Page extends React.Component {
       <div id="page">
         <ContactCard />
         <div id="experience">
-          <WhiteSpace noBreaks="5"></WhiteSpace>
-            <ExperienceCard title="Student" content="Computer Science student at North Dakota State University with a focus on cyber security. Projected graduation: 2022" />
-            <ExperienceCard title="Cyber Security Research" content="Heavily involved with NDSU's Cybersecurity Student Association." />
-            <ExperienceCard title="Software Development" subtitle="Software Developent Intern" content="Employed at Rural Computer Consultants out of Bird Island, MN" />
+          <h1 id="heading" background="#006080">Experience</h1>
+            <ExperienceCard title="Student" content="Computer Science student at North Dakota State University with a focus on cyber security. Projected graduation: 2022" textbackground="darkblue" />
+            <br/>
+            <ExperienceCard title="Cyber Security" textbackground="cyberblue" subtitle="Heavily involved with NDSU's Cybersecurity Student Association." content="Participated in 4 team competitions including CCDC, CPTC, Hivestorm, and NCL" />
+            <br/>
+            <ExperienceCard title="Software Development" textbackground="darkblue" subtitle="Software Development Intern" content="Employed at Rural Computer Consultants out of Bird Island, MN" />
           <WhiteSpace noBreaks="5"></WhiteSpace>
         </div>
+
         <div id="competitions">
-        <WhiteSpace noBreaks="5"></WhiteSpace>
-          <ExperienceCard title="Collegiate Penetration Testing Competition (CPTC) 2019" content="" />
-          <ExperienceCard title="National Collegiate Cyber Defense Competition (CCDC) 2019/2020" content="" />
-          <ExperienceCard title="Hivestorm 2019" content="" />
-          <ExperienceCard title="National Cyber League" content="" />
-        <WhiteSpace noBreaks="5"></WhiteSpace>
+        <h1 id="heading" background-color="861CAB">Competition</h1>
+          <ExperienceCard id="comp" title="Collegiate Penetration Testing Competition (CPTC) 2019" content="Live penetration test on a mock banking network" textbackground="darkred" />
+            <br/>
+          <ExperienceCard title="National Collegiate Cyber Defense Competition (CCDC) 2019/2020" textbackground="lightred" content="Blue team competition that tasked each team with securing and defending a business network" textbackground="lightred" />
+            <br/>
+          <ExperienceCard title="Hivestorm 2019" textbackground="darkred" content="Deffensive competition that challenges each team with securing various machines" />
+            <br/>
+          <ExperienceCard title="National Cyber League" textbackground="lightred" content="Nation-wide competion that tests competitors on various cyber security topics" />
         </div>
+
         <div id="technical">
         </div>
+
         <div id="footer">
         </div>
+
       </div>
     );
   }
@@ -37,7 +45,7 @@ class ContactCard extends React.Component {
     return (
     <div id="ContactCard">
       <WhiteSpace noBreaks="3"></WhiteSpace>
-      <img src={ProfilePic} height="200" border="1px"></img>
+      <img src={ProfilePic} id="pp"></img>
       <h1>Devin Gluesing</h1>
       <h3>North Dakota State University</h3>
       <h3>devingluesing@gmail.com</h3>
@@ -61,6 +69,12 @@ const WhiteSpace = ({noBreaks} : {noBreaks: int}) => {
     );
 }
 
+// const Heading = ({color, content} : {color: string, content: string}) => {
+//   return (
+//     <h1 background={color}>{content}</h1>
+//   );
+// }
+
 class ExperienceCard extends React.Component {
   constructor(props) {
     super(props);
@@ -68,16 +82,17 @@ class ExperienceCard extends React.Component {
       Title: this.props.title,
       SubTitle: this.props.subtitle,
       Content: this.props.content,
+      TextBackground: this.props.textbackground
     }
   }
   render() {
     return (
-      <div>
+      <div id={this.state.TextBackground}>
         <h2>{this.state.Title}</h2>
         <h3>{this.state.SubTitle}</h3>
         <p>{this.state.Content}</p>
-        <br/>
       </div>
+
     );
   }
 }
