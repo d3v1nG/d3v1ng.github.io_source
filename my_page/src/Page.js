f3c5e83d11f300a043a957802107ac06bd75abc3
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2020-01-06 00:33:33 -0600

    technical page

--- my_page/src/Page.js
+++ my_page/src/Page.js
@@ -9,7 +9,7 @@ class Page extends React.Component {
       <div id="page">
         <ContactCard />
         <div id="experience">
-          <h1 id="heading" background="#006080">Experience</h1>
+          <h1 id="heading-blue">Experience</h1>
             <ExperienceCard title="Student" content="Computer Science student at North Dakota State University with a focus on cyber security. Projected graduation: 2022" textbackground="darkblue" />
             <br/>
             <ExperienceCard title="Cyber Security" textbackground="cyberblue" subtitle="Heavily involved with NDSU's Cybersecurity Student Association." content="Participated in 4 team competitions including CCDC, CPTC, Hivestorm, and NCL" />
@@ -19,7 +19,7 @@ class Page extends React.Component {
         </div>
 
         <div id="competitions">
-        <h1 id="heading" background-color="861CAB">Competition</h1>
+        <h1 id="heading-red">Competitions</h1>
           <ExperienceCard id="comp" title="Collegiate Penetration Testing Competition (CPTC) 2019" content="Live penetration test on a mock banking network" textbackground="darkred" />
             <br/>
           <ExperienceCard title="National Collegiate Cyber Defense Competition (CCDC) 2019/2020" textbackground="lightred" content="Blue team competition that tasked each team with securing and defending a business network" textbackground="lightred" />
@@ -30,6 +30,8 @@ class Page extends React.Component {
         </div>
 
         <div id="technical">
+          <h1 id="heading-purp">Technical</h1>
+          <ExperienceList title="Languges" content="Python, C#(DotNet), Java, JavaScript" />
         </div>
 
         <div id="footer">
@@ -69,11 +71,25 @@ const WhiteSpace = ({noBreaks} : {noBreaks: int}) => {
     );
 }
 
-// const Heading = ({color, content} : {color: string, content: string}) => {
-//   return (
-//     <h1 background={color}>{content}</h1>
-//   );
-// }
+class ExperienceList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      Title: this.props.title,
+      Content: this.props.content,
+      TextBackground: this.props.textbackground
+    }
+  }
+  render() {
+    var contentList = this.state.Content.split(",");
+    contentList = contentList.map((item) => <li>{item}</li>);
+    return (
+    <div id={this.state.TextBackground}>
+      <h2>{this.state.Title}</h2>
+        <ul>{contentList}</ul>
+    </div>);
+  }
+}
 
 class ExperienceCard extends React.Component {
   constructor(props) {
